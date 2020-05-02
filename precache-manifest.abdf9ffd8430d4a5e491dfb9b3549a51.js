0113fd82535fdd23bede7f65c391e4f7da665ce3
Harshit Budhraja <harshitbudhraja1301@gmail.com>
2020-05-02 15:28:35 +0530

    Updates

---
+++ precache-manifest.abdf9ffd8430d4a5e491dfb9b3549a51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca693979e2dfd1c3b58a66a53564c5e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d2cedbf5a91277998c1e",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f96744fd96edc904890d",
+    "url": "./static/js/2.bb1ff9a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bb1ff9a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2cedbf5a91277998c1e",
+    "url": "./static/js/main.c9b99c19.chunk.js"
+  },
+  {
+    "revision": "05e99c7faaeb12c3dda2",
+    "url": "./static/js/runtime-main.f9440913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
